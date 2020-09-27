41105a4b23bf6d65dc21eee58a54289c418370d0
Kyle Fasanella <legokyle716@aol.com>
2020-09-27 14:07:07 -0400

    trying to render

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,44 @@ import Footer from './Footer';
 import Project from './Project';
 import About from './About';
 
-function App() {
-  fetch('get-projects.php')
-  .then(response=>response.json())
-  .then(json=>{
-    console.log(json);
-  });
-  
-  return (
-    <div className="App">
-      <Header/>
-      <div id="projects">
-        <Project styleType='ProjectHalfShowTablet' title='NewGroundsProfle' modalNum='37' imageLink="https://raw.githubusercontent.com/fasky/Images/master/PortfolioStuff/dl-tank.gif" secondImage="https://raw.githubusercontent.com/fasky/Images/master/PortfolioStuff/dl-tank.gif" description="<a target='_blank' href='https://fasky.newgrounds.com/'>Click Here.</a>"/>
+// var result = {
+//   styleType: "",
+//   title:"",
+//   modalNum:"",
+//   imageLink:"",
+//   secondImage:"",
+//   description:""
+// };
+var results = [];
 
+class App extends Component {
+
+  render(){
+    var allProjects;
+
+    fetch('get-projects.php')
+    .then(response=>response.json())
+    .then(json=>{
+      results = json;
+      console.log(results);
+      allProjects = this.state.projects.map((project)=>
+        <Project styleType={project.styleType} title={project.title} modalNum={project.modalNum} imageLink={project.imageLink} secondImage={project.secondImage} description={project.description}/>
+      );
+    });  
+
+    return (
+      <div className="App">
+        <Header/>
+        <div id="projects">
+          {allProjects}
+        </div>
+        <div id="aboutSection">
+          <About/>
+        </div>
+        <Footer/>
       </div>
-      <div id="aboutSection">
-        <About/>
-      </div>
-      <Footer/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
