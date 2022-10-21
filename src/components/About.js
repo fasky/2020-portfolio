0c6d5efc0bc088ff14b9f3bee82db37b100acee0
fasky <fasanellaky@gmail.com>
2022-10-21 15:37:51 -0600

    Fix for cloudflare

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,9 @@ function About() {
           <p>Kyle Fasanella is a summer 2020 RIT grad with a B.S. in Game Development.</p>
           <p>He spent two co-ops at RIT as a full-stack web developer.</p>
           <p>Much of his favorite classwork dealt with web development and he is pursuing a career in the field.</p>
-          <p>He also has many game and software projects under his belt, both for class and personal.</p>
-          <p>Currently between opportunities, he spends time working on new websites, climbing, and biking.</p>
-          <p>Originally from CT, he is searching for jobs in the NY metro area, as well as Rochester and Colorado.</p>
+          <p>He has game and software projects under his belt, both for class and personal.</p>
+          <p>Currently between opportunities, he spends time working on new websites, climbing, and modding.</p>
+          <p>Originally from CT, he is searching for jobs near Denver.</p>
           <p>He's open for remote work as well.</p>
           <p>JS, HTML, CSS, Java, React, Angular, Node, C/C++, C#, Unity, Photoshop, Premiere</p>
           <p><a target="_blank" href="https://docs.google.com/document/d/1eKLengVle-ndJy8GgMYuo4oOJJ0o1WupW8j9UnZxM_c/edit?usp=sharing">Résumé</a></p>
